8b3c96760d49787c98a0ee4372d81016f6ee3087
Rahul Singh <rscodedocs@gmail.com>
2020-10-04 23:47:35 +0530

    svelte basic setup

---
+++ basics/src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  let name = 'world !!! 😁';
+
+  let count = 0;
+
+  $: doubled = count * 2;
+
+  // declaring the statements reactively
+  $: console.log('Hello i am doubled value', doubled);
+
+  // grouping multiple statements together
+  $: {
+    console.log('I am group of statements');
+    // alert('count is ' + count);
+  }
+
+  // conditional statements
+  $: if (count > 10) {
+    count = 0;
+  }
+
+  function handleClick() {
+    // console.log('button is clicked');
+    count = count + 1;
+  }
+</script>
+
+<style>
+</style>
+
+<main>
+  <h1>hello {name} and count is {count} and doubled is {doubled}</h1>
+  <button on:click={handleClick}>Clicked</button>
+</main>
